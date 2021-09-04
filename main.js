51dcde291eb58bd166ec0141e19ce087da0809ca
Harald Reingruber <harald.reingruber@gmx.at>
2021-09-04 10:46:19 +0200

    Rename preset parameter to config

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 const { program } = require('commander');
-const {validPresets} = require('./src/constants');
+const {validPresets: validConfigTypes} = require('./src/constants');
 const packageJson = require('./package.json');
 const { createWindowsVm } = require('./src/windows-gce-instance');
 
 program.version(packageJson.version);
 
 program
-  .option('-p, --preset <name>', `Available names: ${validPresets.join(", ")}` )
+  .option('-c, --config <type>', `Available config types: ${validConfigTypes.join(", ")}` )
   // .option('--dry-run', 'show which instance would be created, without doing so')
   // .option('-p, --pizza-type <type>', 'flavour of pizza')
   ;
@@ -17,5 +17,7 @@ program.parse(process.argv);
 program.addHelpCommand();
 
 const options = program.opts();
-if (!validPresets.includes(options.preset)) program.help();
-else createWindowsVm(options.preset);
+if (!validConfigTypes.includes(options.config)) {
+  program.help();
+}
+else createWindowsVm(options.config);
