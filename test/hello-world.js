d1084f11c2c378f9b1bf266b4875bad38c3890fa
koushik-ms <koushik.ms@gmail.com>
2021-09-08 20:31:03 +0000

    Add a first behavioral test
    Co-authored-by: koushik-ms <koushik.ms@gmail.com>
    Co-authored-by: Harald Reingruber <haraldreingruber@users.noreply.github.com>
    Co-authored-by: Jose Padilla <emailreel@gmail.com>

--- test/hello-world.js
+++ test/hello-world.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+const { createWindowsVm } = require('../src/windows-gce-instance');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
@@ -6,3 +7,30 @@ describe('Array', function() {
     });
   });
 });
+
+/*
+Let's write a behavioral test using Given-When-Then
+Test 0
+   given the vanilla configuration
+   when the user requests a VM
+   then a windows VM is created
+   and a display has been activated on the VM
+   and a bunch of recommended programs including chocolately are installed on the VM at startup
+*/
+describe('Vanilla Configuration', function() {
+  it('should create a Windows VM', async function() {
+    const { createWindowsVm, getVanillaConfig } = require('../src/windows-gce-instance');
+    process.env['GOOGLE_APPLICATION_CREDENTIALS'] = 'dummy'
+    vm = getVanillaConfig();
+    console.log(vm.config);
+    assert(vm.config.os == 'windows');
+    // assert vm.display == true
+    // assert vm.scripts['windows-startup-script-ps1'] contains ['chocolatey', 'git']
+  });
+  
+});
+/*
+options to make the test pass:
+1. make the function-under-test return the config as a member of its result (object)
+2. make a createWindowsVMConfig function that returns only the config
+*/
